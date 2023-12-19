465828141ab904e1e99ef770134cdbfa9efa2aa4
jinchen <278229887@qq.com>
2023-12-19 17:52:37 +0800

    db还原

--- utils/dbtools.go
+++ utils/dbtools.go
@@ -6,7 +6,7 @@ import (
 )
 
 func InitDB() *gorm.DB {
-	dsn := "root:48mysqljc.@tcp(1.94.40.252:3306)/research_platform_go?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:zz.@tcp(zz:3306)/zz?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database, err:" + err.Error())
