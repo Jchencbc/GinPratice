ebd321eee5165a0fed2264b6e0f19af559c15cc0
jinchen <278229887@qq.com>
2023-12-19 17:51:36 +0800

    文件上传工具类封装，消息相关表初始化

--- utils/dbtools.go
+++ utils/dbtools.go
@@ -1,19 +1,16 @@
 package utils
 
 import (
-	"ginPratice/models"
-
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-
 func InitDB() *gorm.DB {
-	dsn := "root:zz.@tcp(zz:3306)/research_platform_go?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:48mysqljc.@tcp(1.94.40.252:3306)/research_platform_go?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database, err:" + err.Error())
 	}
-	db.AutoMigrate(&models.User{}) //迁移model层
+	// db.AutoMigrate(&models.User{}) //迁移model层
 	return db
 }
